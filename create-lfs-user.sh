027776ca8f5a023c5aa9b3def6efbf249a597b40
Ethan Corey <ethanscorey@gmail.com>
2022-09-02 19:15:47 +0000

    New script for creating lfs user

---
+++ create-lfs-user.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+LFS_USER=$1
+export LFS=/mnt/lfs
+export LFS_TGT=x86_64-lfs-linux-gnu
+
+if ! [ $USER == $LFS_USER ]; then
+    if ! grep -q $LFS_USER /etc/passwd;then
+        sudo useradd -s /bin/bash -d /home/$LFS_USER -k /dev/null -mU $LFS_USER
+        sudo passwd -d $LFS_USER
+        sudo usermod -aG sudo $LFS_USER
+        sudo cp -rf . /home/$LFS_USER/lfs
+        sudo cp -f ./lfs_bashrc /home/$LFS_USER/.bashrc
+        sudo sed -e "s|\$LFS_TGT|$LFS_TGT|" -i /home/$LFS_USER/.bashrc
+        sudo sed -e "s|\$LFS|$LFS|" -i /home/$LFS_USER/.bashrc
+        sudo cp -f ./lfs_bash_profile /home/$LFS_USER/.bash_profile
+        sudo sed -e "s|\$HOME|/home/$LFS_USER|" -i /home/$LFS_USER/.bash_profile
+        sudo sed -e "s|\$TERM|$TERM|" -i /home/$LFS_USER/.bash_profile
+        sudo cp -f ./lfs_vimrc /home/$LFS_USER/.vimrc
+        sudo chown -R $LFS_USER:$LFS_USER /home/$LFS_USER
+    fi
+    su - $LFS_USER
+fi
--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 export LFS=/mnt/LFS
 export LFS_TGT=x86_64-lfs-linux-gnu
 export LFS_DISK=$1
-export LFS_USER_PW=$2
 
 source install-development-packages.sh
 
@@ -38,19 +37,3 @@ case $(uname -m) in
     x86_64) mkdir -pv $LFS/lib64 ;;
 esac
 mkdir -pv $LFS/tools
-
-# Add the LFS user if it doesn't exist and give ownership of files
-if ! grep -q lfs /etc/passwd; then
-	addgroup lfs
-	adduser -s /bin/bash -G lfs -h /home/lfs -k /dev/null lfs
-	passwd lfs << EOF
-$LFS_USER_PW
-$LFS_USER_PW
-EOF
-chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools,sources}
-case $(uname -m) in
-    x86_64) chown -v lfs $LFS/lib64 ;;
-esac
-fi
-source /lfs/setup-env.sh $LFS_DISK $LFS_USER_PW
-su - lfs
