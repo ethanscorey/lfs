d49341a8b9d37194277dbcffa28bec656bda6151
Ethan Corey <ethanscorey@gmail.com>
2022-09-02 16:10:15 +0000

    Fixed script to install github-cli

--- install-development-packages.sh
+++ install-development-packages.sh
@@ -8,6 +8,7 @@ sudo apt install -y \
     bison \
     build-essential \
     coreutils \
+    curl \
     diffutils \
     e2fsprogs \
     findutils \
@@ -27,7 +28,13 @@ sudo apt install -y \
     texinfo \
     xz-utils
 sudo ln -svf bash /bin/sh
-sudo apt install -y github-cli vim
+GH_KEYRING=/usr/share/keyrings/githubcli-archive-keyring.gpg
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=$GH_KEYRING \
+    && sudo chmod go+r $GH_KEYRING \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=$GH_KEYRING] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+sudo apt install -y vim
 cat > ~/.vimrc << EOF
 filetype plugin indent on
 inoremap jk <ESC>
