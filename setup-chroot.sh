a687cb0427878fbf6482b3fbf2dc580abffba878
Ethan Corey <ethanscorey@gmail.com>
2022-09-05 22:49:05 +0000

    Made mount commands conditional in setup-chroot.sh

--- setup-chroot.sh
+++ setup-chroot.sh
@@ -10,11 +10,21 @@ case $(uname -m) in
 	x86_64) chown -R root:root $LFS/lib64 ;;
 esac
 mkdir -pv $LFS/{dev,proc,sys,run}
-mount -v --bind /dev $LFS/dev
-mount -v --bind /dev/pts $LFS/dev/pts
-mount -vt proc proc $LFS/proc
-mount -vt sysfs sysfs $LFS/sys
-mount -vt tmpfs tmps $LFS/run
+if ! grep "$LFS/dev" /proc/mounts; then
+    mount -v --bind /dev $LFS/dev
+fi
+if ! grep "$LFS/dev/pts" /proc/mounts; then
+    mount -v --bind /dev/pts $LFS/dev/pts
+fi
+if ! grep "$LFS/proc" /proc/mounts; then
+    mount -vt proc proc $LFS/proc
+fi
+if ! grep "$LFS/sys" /proc/mounts; then
+    mount -vt sysfs sysfs $LFS/sys
+fi
+if ! grep "$LFS/run" /proc/mounts; then
+    mount -vt tmpfs tmps $LFS/run
+fi
 if [ -h $LFS/dev/shm ]; then
 	mkdir -pv $LFS/$(readlink $LFS/dev/shm)
 fi
@@ -29,3 +39,4 @@ for script in "inside-chroot.sh" "inside-chroot2.sh"; do
         PS1='(lfs chroot) \u:\w$ ' \
         PATH=/usr/bin:/usr/sbin \
         /bin/bash --login +h -c /sources/$script
+done
