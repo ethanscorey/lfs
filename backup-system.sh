7aedee68d6a7d8130a00a8354460de17c9f32f6d
Ethan Corey <ethanscorey@gmail.com>
2022-09-07 05:00:30 +0000

    Updated system backup script to remount directories after archiving is finished.

--- backup-system.sh
+++ backup-system.sh
@@ -5,5 +5,14 @@ if [ "$LFS" == "" ]; then
 fi
 sudo umount $LFS/dev/pts
 sudo umount $LFS/{sys,proc,run,dev}
-cd $LFS
+pushd $LFS
 tar -cJpf $HOME/lfs-temp-tools-11.2.tar.xz .
+popd
+sudo mount -v --bind /dev $LFS/dev
+sudo mount -v --bind /dev/pts $LFS/dev/pts
+sudo mount -vt proc proc $LFS/proc
+sudo mount -vt sysfs sysfs $LFS/sys
+sudo mount -vt tmpfs tmps $LFS/run
+if [ -h $LFS/dev/shm ]; then
+	mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+fi
