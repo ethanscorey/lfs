5668ab7577f96a337bd173c8051362d18b187dbc
Ethan Corey <ethanscorey@gmail.com>
2022-09-07 04:41:25 +0000

    Added sudo to backup-system.sh

--- backup-system.sh
+++ backup-system.sh
@@ -3,7 +3,7 @@ if [ "$LFS" == "" ]; then
     echo "LFS not defined"
     exit i
 fi
-umount $LFS/dev/pts
-umount $LFS/{sys,proc,run,dev}
+sudo umount $LFS/dev/pts
+sudo umount $LFS/{sys,proc,run,dev}
 cd $LFS
 tar -cJpf $HOME/lfs-temp-tools-11.2.tar.xz .
