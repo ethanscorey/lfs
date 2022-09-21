5b4d2d78f795471c90b56be10f7adc252e3b66ba
Ethan Corey <ethanscorey@gmail.com>
2022-09-21 17:08:51 +0000

    Updated cleanup to remove tester home directory

--- installation-cleanup.sh
+++ installation-cleanup.sh
@@ -3,4 +3,6 @@ rm -rf /tmp/*
 find /usr/lib /usr/libexec -name \*.la -delete
 find /usr -depth -name $(uname -m)-lfs-linux-gnu\* | xargs rm -rf
 userdel -r tester
+# tester doesn't own its home directory, so we need to manually delete
+rm -rf /home/tester
 exit
