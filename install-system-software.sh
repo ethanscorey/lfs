dd47c3866b71cdd2881388c6a2e779d26230ce7a
Ethan Corey <ethanscorey@gmail.com>
2022-09-21 17:46:41 +0000

    Refactored to encapsulate run-in-chroot logic

--- install-system-software.sh
+++ install-system-software.sh
@@ -4,35 +4,8 @@ if [ "$LFS" == "" ]; then
     echo "LFS is not defined."
     exit 1
 fi
-
-sudo chown -R root:root $LFS/{usr,lib,var,etc,bin,sbin}
-case $(uname -m) in
-	x86_64) sudo chown -R root:root $LFS/lib64 ;;
-esac
-if ! grep "$LFS/dev" /proc/mounts; then
-    sudo mount -v --bind /dev $LFS/dev
-fi
-if ! grep "$LFS/dev/pts" /proc/mounts; then
-    sudo mount -v --bind /dev/pts $LFS/dev/pts
-fi
-if ! grep "$LFS/proc" /proc/mounts; then
-    sudo mount -vt proc proc $LFS/proc
-fi
-if ! grep "$LFS/sys" /proc/mounts; then
-    sudo mount -vt sysfs sysfs $LFS/sys
-fi
-if ! grep "$LFS/run" /proc/mounts; then
-    sudo mount -vt tmpfs tmps $LFS/run
-fi
-if [ -h $LFS/dev/shm ]; then
-	sudo mkdir -pv $LFS/$(readlink $LFS/dev/shm)
-fi
-chmod ugo+x chapter8/install-packages.sh
-sudo mkdir -pv $LFS/sources/lfs
-sudo cp -rf . $LFS/sources/lfs
-sudo chroot "$LFS" /usr/bin/env -i \
-    HOME=/root \
-    TERM="$TERM" \
-    PS1='(lfs chroot) \u:\w$ ' \
-    PATH=/usr/bin:/usr/sbin \
-    /bin/bash --login +h -c "/sources/lfs/chapter8/install-packages.sh"
+for script in \
+    "chapter8/install-packages.sh" \
+    "installation-cleanup.sh" \
+    "system-config.sh"; do
+    ./run-in-chroot.sh $LFS $script
--- setup-chroot.sh
+++ setup-chroot.sh
@@ -1,50 +1,11 @@
 #!/bin/bash
-# Set up the chroot environment
 export LFS=$1
 if [ "$LFS" == "" ]; then
     echo "LFS is not defined."
     exit 1
 fi
-chown -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
-case $(uname -m) in
-	x86_64) chown -R root:root $LFS/lib64 ;;
-esac
-mkdir -pv $LFS/{dev,proc,sys,run}
-if ! grep "$LFS/dev" /proc/mounts; then
-    mount -v --bind /dev $LFS/dev
-fi
-if ! grep "$LFS/dev/pts" /proc/mounts; then
-    mount -v --bind /dev/pts $LFS/dev/pts
-fi
-if ! grep "$LFS/proc" /proc/mounts; then
-    mount -vt proc proc $LFS/proc
-fi
-if ! grep "$LFS/sys" /proc/mounts; then
-    mount -vt sysfs sysfs $LFS/sys
-fi
-if ! grep "$LFS/run" /proc/mounts; then
-    mount -vt tmpfs tmps $LFS/run
-fi
-if [ -h $LFS/dev/shm ]; then
-	mkdir -pv $LFS/$(readlink $LFS/dev/shm)
-fi
-chmod ugo+x inside-chroot.sh
-chmod ugo+x inside-chroot2.sh
-chmod ugo+x inside-chroot3.sh
-mkdir -pv $LFS/sources/lfs
-cp -rf . $LFS/sources/lfs
 for script in \
     "lfs/inside-chroot.sh" \
     "lfs/inside-chroot2.sh" \
     "lfs/inside-chroot3.sh"; do
-    chroot "$LFS" /usr/bin/env -i \
-        HOME=/root \
-        TERM="$TERM" \
-        PS1='(lfs chroot) \u:\w$ ' \
-        PATH=/usr/bin:/usr/sbin \
-        /bin/bash --login +h -c "/sources/$script"
-done
-chown -R lfs:lfs $LFS/{usr,lib,var,etc,bin,sbin}
-case $(uname -m) in
-	x86_64) chown -R lfs:lfs $LFS/lib64 ;;
-esac
+    ./run-in-chroot.sh $LFS $script
